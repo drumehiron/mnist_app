8a21fe672a846feb79efd2a9a4132854d4692542
Hiroshi Umeki <drumehiron@gmail.com>
2020-11-18 20:03:02 +0900

    production_ver

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import (
     Flask,
     render_template,
 )
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
@@ -11,4 +13,6 @@ def root():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
