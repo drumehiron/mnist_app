f3b54de7dad0186043222a8a3ae31805e0ec10a4
Hiroshi Umeki <drumehiron@gmail.com>
2020-11-18 21:16:42 +0900

    fix/producion

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def root():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True)
-    http_server = WSGIServer(('', 5000), app)
+    http_server = WSGIServer((''), app)
     http_server.serve_forever()
